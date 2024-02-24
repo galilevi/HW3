1ec968b9db882399782345db84699dbff2bad6b3
galilevi <gali25022@gmail.com>
2024-02-24 22:57:48 +0200

    login page part 1

--- login.js
+++ login.js
@@ -1,4 +1,21 @@
+let visitorsForView = [...visitors];
+const dialog = document.querySelector("#visitor-dialog");
+
+const getVisitorHTMLCard = (visitor) => {
+  const template = `
+      <div class="card" style="min-height: 360px;" >
+        <img class="card-img-top" src="${visitor.image}" alt="${visitor.name}"/>
+        <div class="card-body">
+          <p class="card-text">${visitor.name}</p>
+          <p class="card-text">Coins: ${visitor.coins}</p>
+        </div>
+      </div>`;
+      
+
 function loginAsVisitor(visitorName) {
   // תממשו את הלוגיקה של בחירת אורח שנכנס לגן החיות
   // שמרו את האורח שבחרתם, בלוקל סטורג' כך שבכל העמודים נדע מי האורח הנוכחי
+
+
+}
 }
