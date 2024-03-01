8de0d8012b30047e3b43a34afc8ea71ec55453f5
galilevi <gali25022@gmail.com>
2024-03-01 15:54:33 +0200

    UPDATE

--- login.js
+++ login.js
@@ -15,7 +15,7 @@ const wrapper = document.createElement("div")
 wrapper.className= "visitor-card";
 wrapper.innerHTML = template;
 
-wrapper.addEventListener("click", () => loginAsVisitor (visitor.name));
+wrapper.addEventListener("click", () => handleVisitorClick (visitor));
 return wrapper;
 };
 
@@ -24,11 +24,71 @@ const getCloseModalHTMLButton = () => {
   closeButton.innerText="close modal";
   closeButton.addEventListener("click",()=> dialog.close ())
   return closeButton;
-}
+};
+
+const handleVisitorClick = (visitor) => {
+  dialog.innerHTML = "";
+  dialog.append(getCloseModalHTMLButton(), getVisitorHTMLCard(visitor));
+  dialog.showModal();
+};
+
+const getSearchBox = () => {
+  const queryInput = document.createElement("input");
+  queryInput.id = "query-input";
+  queryInput.placeholder = "Search visitor";
+  queryInput.className = "form-control my-4";
+  queryInput.oninput = (e) => {
+    visitorsForView = visitors.filter((visitor) =>
+    visitor.name.includes(e.target.value));
+    renderVisitors();};
+  return queryInput;
+};
+
+const getEmptyCardsHTMLTemplate = () => {
+  const templateWrapper = document.createElement("div");
+  templateWrapper.className = "empty-result";
+
+  const template = `
+    <h2>No visitor Found</h2>
+    <p>We're sorry, but no visitors match your search or filter criteria.</p>
+    <button id="clear-filter-btn" class="btn btn-dark">Clear search text</button>`;
+
+  templateWrapper.innerHTML = template;
+  templateWrapper.children["clear-filter-btn"].addEventListener("click",clearSearchBox);
+  return templateWrapper;
+};
+
+const clearSearchBox = () => {
+  const input = document.getElementById("query-input");
+  input.value = "";
+  visitorsForView = [...visitors];
+  renderVisitors();
+};
+
+const renderVisitors = () => {
+  const visitorCards = visitorsForView.map(getVisitorHTMLCard);
+  const visitorsPlaceholder = document.getElementById("place-holder");
+  visitorsPlaceholder.innerHTML = "";
+
+  if (!visitorCards.length) {
+    visitorsPlaceholder.appendChild(getEmptyCardsHTMLTemplate());
+  }
+  visitorsPlaceholder.append(...visitorCards);
+};
+
+document.body.insertAdjacentElement("afterbegin", getSearchBox());
+window.addEventListener("load", renderVisitors);
+
+
+
+
+
+
 
 function loginAsVisitor(visitorName) {
   // תממשו את הלוגיקה של בחירת אורח שנכנס לגן החיות
   // שמרו את האורח שבחרתם, בלוקל סטורג' כך שבכל העמודים נדע מי האורח הנוכחי
+
 
 
 }
